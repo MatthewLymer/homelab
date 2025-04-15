8854cd45406c80597dc26c7b319e28822c0222c9
Matthew Lymer <matthew@lymer.ca>
2025-04-14 22:45:43 -0400

    Get homelink cron running

--- homelink/cron.sh
+++ homelink/cron.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile.d/python3.bash
 
-GOOGLE_APPLICATION_CREDENTIALS=/share/homes/admin/homelink/matthewlymer-production-b2e5ab52c4ad.json \
+CLOUDSDK_AUTH_CREDENTIAL_FILE_OVERRIDE=/share/homes/admin/homelink/matthewlymer-production-b2e5ab52c4ad.json \
     GOOGLE_PROJECT=matthewlymer-production \
     PATH="${PATH}:/share/homes/admin/google-cloud-sdk/bin" \
-    source update-dns-records.sh
+    source /share/homes/admin/homelink/update-dns-records.sh
---
+++ homelink/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# see https://wiki.qnap.com/wiki/Add_items_to_crontab
+# for documentation on QNAP cron implementation
+#
+
+SCRIPT_PATH=/share/homes/admin/homelink/cron.sh
+CRONTAB_PATH=/etc/config/crontab
+
+if [[ "$(cat $CRONTAB_PATH)" != *"$SCRIPT_PATH"* ]]; then
+    # run every 15 minutes, offset by some random amount
+    echo "H/15 * * * * /share/homes/admin/homelink/cron.sh >> ${SCRIPT_PATH}.log" >> $CRONTAB_PATH
+
+    # restart crontab
+    crontab /etc/config/crontab && /etc/init.d/crond.sh restart
+fi

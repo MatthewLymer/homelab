589a47b75ea2119b8447c13d0a5ee54a3b5c74d7
Matthew Lymer <matthew@lymer.ca>
2025-05-04 15:33:13 -0400

    Get publish script working

--- publish.sh
+++ publish.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-function fail() {
-    exit 1
-}
-
-trap 'fail' ERR
-
-CONTAINER_NAME=$(uuidgen)
 SSH_CREDS="qnappy@qnappy"
-
-export DOCKER_HOST="ssh://${SSH_CREDS}"
-
 GOOGLE_PROJECT="matthewlymer-production"
 
-REMOTE_HOME=$(ssh $SSH_CREDS -C 'echo $HOME')
+# aliases
+dc() {
+    DOCKER_HOST="ssh://${SSH_CREDS}" \
+    docker compose \
+    -f docker-compose.yml \
+    -f docker-compose.production.yml \
+    "$@"
+}
 
-# docker run --detach --rm --name $CONTAINER_NAME --volume "${REMOTE_HOME}/home-automation:/app" alpine sh -c 'sleep 5m'
-
-# echo "attack at dawn" | docker exec -i $CONTAINER_NAME sh -c 'cat > /app/priv.txt'
+echo "Pushing config changes."
 
 ssh $SSH_CREDS -C "[ -d ~/home-automation ] || mkdir ~/home-automation"
+gcloud secrets versions access latest --project=$GOOGLE_PROJECT --secret=certbot_sa_key | ssh $SSH_CREDS -C 'cat - > ~/home-automation/certbot_sa_key.json && chmod 600 ~/home-automation/certbot_sa_key.json'
+gcloud secrets versions access latest --project=$GOOGLE_PROJECT --secret=homelink_sa_key | ssh $SSH_CREDS -C 'cat - > ~/home-automation/homelink_sa_key.json && chmod 600 ~/home-automation/homelink_sa_key.json'
 
-echo "You know you want it" | ssh $SSH_CREDS -C 'cat - > ~/home-automation/goo.txt'
+cat ./workloads/nginx/nginx.conf | ssh $SSH_CREDS -C "cat - > ~/home-automation/nginx.conf"
 
-# docker ps
+echo "Starting workloads."
 
-# gcloud secrets versions access latest --project=$GOOGLE_PROJECT --secret=certbot_sa_key 2>/dev/null
+dc up --detach
+
+echo "Reloading nginx."
+
+dc kill --signal HUP nginx
+
+echo "All Done."

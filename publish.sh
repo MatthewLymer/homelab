842001843c6ae39eabbdd57430aa63ac9153b611
Matthew Lymer <matthew@lymer.ca>
2025-05-11 22:36:20 -0400

    Add actual budget, add http to https redirect

--- publish.sh
+++ publish.sh
@@ -22,9 +22,13 @@ sshq -C "[ -d ~/home-automation ] || mkdir ~/home-automation"
 gcloud secrets versions access latest --project=$GOOGLE_PROJECT --secret=certbot_sa_key | sshq -C 'cat - > ~/home-automation/certbot_sa_key.json && chmod 600 ~/home-automation/certbot_sa_key.json'
 gcloud secrets versions access latest --project=$GOOGLE_PROJECT --secret=homelink_sa_key | sshq -C 'cat - > ~/home-automation/homelink_sa_key.json && chmod 600 ~/home-automation/homelink_sa_key.json'
 
+# nginx
 sshq -C "[ -d ~/home-automation/nginx ] || mkdir ~/home-automation/nginx"
 sshq -C 'find ~/home-automation/nginx -type f -delete' # delete files but keep directories
 tar -C ./workloads/nginx -cf - . | sshq -C 'tar -C ~/home-automation/nginx -xf -'
+
+# actual
+sshq -C "([ -d ~/home-automation/actual ] || mkdir ~/home-automation/actual) && [ -d ~/home-automation/actual/data ] || mkdir ~/home-automation/actual/data"
 
 echo "Starting workloads."
 
